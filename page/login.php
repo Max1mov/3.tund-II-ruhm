0fa4e621beb6d682f205a9269567c517f35744b8
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-18 12:58:51 +0300

    separete form validation

--- page/login.php
+++ page/login.php
@@ -10,22 +10,46 @@
 	//kontrollin kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
-		//echo "jah";
 		
-		// kas e-post on tühi
-		if( empty($_POST["email"]) ) {
+		// kontrollin mis nuppu vajutati
+		if(isset($_POST["login"])){
 			
-			// jah oli tühi
-			$email_error = "See väli on kohustuslik";
+			// ********************
+			// *** LOGIN NUPP *****
+			// ********************
 			
-		}
+			// kas e-post on tühi
+			if( empty($_POST["email"]) ) {
+				
+				// jah oli tühi
+				$email_error = "See väli on kohustuslik";
+				
+			}
+			
+			// kas parool on tühi
+			if( empty($_POST["password"]) ) {
+				
+				// jah oli tühi
+				$password_error = "See väli on kohustuslik";
+				
+			}
 		
-		// kas parool on tühi
-		if( empty($_POST["password"]) ) {
+		
+		} elseif(isset($_POST["create"])){
+		
+			// ********************
+			// *** CREATE NUPP ****
+			// ********************
 			
-			// jah oli tühi
-			$password_error = "See väli on kohustuslik";
-			
+			// kas e-post on tühi
+			if( empty($_POST["name"]) ) {
+				
+				// jah oli tühi
+				$name_error = "See väli on kohustuslik";
+				
+			}
+		
+		
 		}
 		
 	}
@@ -40,14 +64,14 @@
 	<form action="login.php" method="post">
 		<input name="email" type="email" placeholder="E-post" > <?php echo $email_error; ?><br><br>
 		<input name="password" type="password" placeholder="Parool" > <?php echo $password_error; ?> <br><br>
-		<input type="submit" value="Logi sisse" > <br><br>
+		<input name="login" type="submit" value="Logi sisse" > <br><br>
 	</form>
 	
 	
 	<h2>Create user</h2>
 	<form action="login.php" method="post">
 		<input name="name" type="text" placeholder="Eesnimi Perenimi" > <?php echo $name_error; ?><br><br>
-		<input type="submit" value="Loo kasutaja" > <br><br>
+		<input name="create" type="submit" value="Loo kasutaja" > <br><br>
 	</form>
-	
+	 
 <?php require_once("../footer.php"); ?>
